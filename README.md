8542c6ecabf0aba296f70ff1942863d8fc7d870c
noeyhhh <lunihaoNB@outlook.com>
2022-09-16 20:22:38 +0800

    init

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
-# vue-h5-template
+# dota2
 
-基于 vue3 + vite + （nutui or varlet or vant） + sass + viewport 适配方案 + axios 封装，构建手机端模板脚手架
+## Project setup
+```
+yarn install
+```
 
-如果你不熟悉 vue3，想继续使用 vue2 开发的，可以[点这里](https://github.com/sunniejs/vue-h5-template/tree/vue2-h5-template)来获取 vue2-h5-template
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
 
-详细的文档请[点击](https://sunniejs.github.io/vue-h5-template/)查看
-项目初始化
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <Suspense>
-    <template #default>
-      <router-view v-slot="{ Component, route }">
-        <keep-alive>
-          <component :is="Component" v-if="route.meta && route.meta.keepAlive" :key="route.meta.usePathKey ? route.fullPath : undefined" />
-        </keep-alive>
-        <component :is="Component" v-if="!(route.meta && route.meta.keepAlive)" :key="route.meta.usePathKey ? route.fullPath : undefined" />
-      </router-view>
-    </template>
-    <template #fallback> Loading... </template>
-  </Suspense>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
-<script setup></script>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
