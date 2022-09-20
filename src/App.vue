7bbb5d45964e7ec36d851580ac56acb020d4949b
“noeyhhh” <lunihaoNB@outlook.com>
2022-09-20 17:54:34 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,19 @@
 <template>
-  <div class="demo">
-    <button
-        v-for="(_,tab)  in tabs"
-        :key="tab"
-        @click=" currentTab = tab"
-    >
-      {{ tab }}
-    </button>
-    <component :is='tabs[currentTab]' />
-  </div>
+  <input type="text" v-focus>
 </template>
 
 <script setup>
 import ComponentTest from "@/components/ComponentTest";
-import Home from './components/Home'
-import Posts from './components/Posts'
-import Archive from './components/Archive'
+// import Home from './components/Home'
+// import Posts from './components/Posts'
+// import Archive from './components/Archive'
 import {ref} from "vue";
-const tabs = {
-  Home,
-  Posts,
-  Archive
+import { provide } from "vue";
+const vFocus = {
+  mounted: (el) => el.focus()
 }
-const currentTab = ref('Home')
-
-
 </script>
 
 <style>
-.demo {
-  font-family: sans-serif;
-  border: 1px solid #eee;
-  border-radius: 2px;
-  padding: 20px 30px;
-  margin-top: 1em;
-  margin-bottom: 40px;
-  user-select: none;
-  overflow-x: auto;
-}
 
-.tab-button {
-  padding: 6px 10px;
-  border-top-left-radius: 3px;
-  border-top-right-radius: 3px;
-  border: 1px solid #ccc;
-  cursor: pointer;
-  background: #f0f0f0;
-  margin-bottom: -1px;
-  margin-right: -1px;
-}
-.tab-button:hover {
-  background: #e0e0e0;
-}
-.tab-button.active {
-  background: #e0e0e0;
-}
-.tab {
-  border: 1px solid #ccc;
-  padding: 10px;
-}
 </style>
--- src/components/ComponentTest.vue
+++ src/components/ComponentTest.vue
@@ -1,10 +1,25 @@
 <template>
- <h3>Error</h3>
-  <slot></slot>
+  <slot :x="x" :y="y"></slot>
 </template>
+<script>
 
+</script>
 <script setup>
-
+import Home from './Home'
+import {onMounted, onUnmounted, ref} from "vue";
+//
+const x = ref(0)
+const y = ref(0)
+const value = (e) => {
+  x.value = e.pageX
+  y.value = e.pageY
+}
+onMounted( ()=>{
+  window.addEventListener('mousemove',value)
+})
+onUnmounted( ()=>{
+  window.addEventListener('mousemove',value)
+})
 </script>
 
 <style scoped>
