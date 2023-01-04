166a18b8d722ae01d6afc8555847c75e32faa254
noeyhhh <lunihaoNB@outlook.com>
2023-01-04 17:44:59 +0800

    modules

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,12 @@
 import './style/index.less'
-console.log(1111)
-// food类
-class Food{
-    element:HTMLElement;
-    constructor() {
-        this.element = document.getElementById('food')!;
-    }
-    get X(){
-        return this.element.offsetLeft
-    }
-    get Y(){
-        return this.element.offsetTop
-    }
-    change(){
-        const left = Math.round(Math.random()*29)*10
-        const top = Math.round(Math.random()*29)*10
-        this.element.style.left = left+'px'
-        this.element.style.top = top+'px'
-    }
-}
-// 分数类
-class ScorePanel{
-    score = 1;
-    level = 0;
-    // 最大等级
-    maxLevel:number;
-    // 分数和等级所在的元素,在构造函数中进行初始化
-    scoreEle:HTMLElement
-    levelEle:HTMLElement
-    constructor(maxLevel:number = 10) {
-        this.scoreEle = document.getElementById('score')!
-        this.levelEle = document.getElementById('level')!
-        this.maxLevel = maxLevel
-    }
-    addScore(){
-        this.scoreEle.innerHTML = ++this.score + ''
-    }
-    levelUp(){
-        if(this.maxLevel < 10){
-            console.log(123)
-            this.levelEle.innerHTML = ++this.level + ''
-        }
+import Food from './modules/Food'
+import ScorePanel from './modules/ScorePanel'
 
-    }
-}
 
 const food = new Food()
 food.change()
 
-const scorePanel = new ScorePanel()
-scorePanel.addScore()
-scorePanel.addScore()
-scorePanel.levelUp()
-scorePanel.levelUp()
+const scorePanel = new ScorePanel(100,5)
+for(let i=0; i<500; i++){
+    scorePanel.addScore()
+}
---
+++ src/modules/ScorePanel.ts
@@ -0,0 +1,29 @@
+// 分数类
+class ScorePanel{
+    score = 0;
+    level = 0;
+    // 最大等级
+    maxLevel:number;
+    upScore: number;
+    // 分数和等级所在的元素,在构造函数中进行初始化
+    scoreEle:HTMLElement
+    levelEle:HTMLElement
+    constructor(maxLevel:number = 10,upScore = 2) {
+        this.scoreEle = document.getElementById('score')!
+        this.levelEle = document.getElementById('level')!
+        this.maxLevel = maxLevel
+        this.upScore = upScore
+    }
+    addScore(){
+        this.scoreEle.innerHTML = ++this.score + ''
+        if(this.score % this.upScore === 0){
+            this.levelUp()
+        }
+    }
+    levelUp(){
+        if( this.level<this.maxLevel){
+            this.levelEle.innerHTML = ++this.level + ''
+        }
+    }
+}
+export default ScorePanel
