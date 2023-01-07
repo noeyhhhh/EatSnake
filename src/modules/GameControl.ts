6f0f96cbcdcbd946551715f7fb190ddf4b86be2d
Noeyhhh <lunihaoNB@outlook.com>
2023-01-07 22:50:07 +0800

    123

---
+++ src/modules/GameControl.ts
@@ -0,0 +1,4 @@
+//引入其他类
+import Snake from './Snake'
+import Food from './Food'
+import ScorePanel from "./ScorePanel";
--- src/modules/Snake.ts
+++ src/modules/Snake.ts
@@ -27,4 +27,5 @@ class Snake{
         this.element.insertAdjacentHTML('beforeend',"<div></div>")
     }
 }
+export default Snake
 
