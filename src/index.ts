ed8a4ad07bf34775826eedd71bae566d88cc0624
Noeyhhh <lunihaoNB@outlook.com>
2023-01-08 16:58:41 +0800

    123

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 import './style/index.less'
-import Food from './modules/Food'
-import ScorePanel from './modules/ScorePanel'
-
-
-const food = new Food()
-food.change()
-
-const scorePanel = new ScorePanel(100,5)
-for(let i=0; i<500; i++){
-    scorePanel.addScore()
-}
+import GameControl from "./modules/GameControl";
+const gameControl = new GameControl()
+console.log(gameControl.direction)
--- src/modules/GameControl.ts
+++ src/modules/GameControl.ts
@@ -2,3 +2,23 @@
 import Snake from './Snake'
 import Food from './Food'
 import ScorePanel from "./ScorePanel";
+class GameControl{
+    snake:Snake
+    food:Food
+    scorepanel:ScorePanel
+    direction:string = ''
+    constructor() {
+        this.snake = new Snake()
+        this.food = new Food()
+        this.scorepanel = new ScorePanel()
+        this.init()
+    }
+    init(){
+        document.addEventListener('keydown',this.keyDownHandler.bind(this))
+    }
+    keyDownHandler(event:KeyboardEvent){
+        this.direction = event.key
+        console.log(this.direction)
+    }
+}
+export default GameControl
