036ab4f83fed95584bc6ac638d6c5c7450cc381f
Noeyhhh <lunihaoNB@outlook.com>
2023-01-10 22:46:53 +0800

    123

--- review/src/index.ts
+++
@@ -1,2 +0,0 @@
-import './style/index.less'
-import Food from './modules/food'
--- review/src/modules/snake.ts
+++
@@ -1,8 +0,0 @@
-class Snake{
-    snakeHead:HTMLElement
-    snakeBodies:HTMLCollection
-    constructor() {
-        this.snakeHead = document.querySelector('#snake>div') as HTMLElement
-        this.snakeBodies = document.getElementById('snake')!.getElementsByTagName('div')
-    }
-}
