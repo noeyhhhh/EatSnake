ca6949396a02978f1500eb3dd7d6789091350e2d
Noeyhhh <lunihaoNB@outlook.com>
2023-01-11 23:00:18 +0800

    123

--- review/src/index.ts
+++ review/src/index.ts
@@ -1,4 +1,5 @@
-import './style/index.less'
-'
+import './style/index.less';
+import GameControl from './modules/gameControl'
+const gamecontrol = new GameControl()
 
 
--- review/src/modules/gameControl.ts
+++ review/src/modules/gameControl.ts
@@ -1,6 +1,25 @@
 import Food from './food'
 import scorePanel from './scorePanel'
 import Snake from './snake'
+import * as events from "events";
 class GameControl{
-    
+    food:Food
+    scorepanel:scorePanel
+    snake:Snake
+    direction:string = ''
+    constructor() {
+        this.food = new Food()
+        this.scorepanel = new scorePanel()
+        this.snake = new Snake()
+        this.initGame()
+
+    }
+    initGame(){
+        document.addEventListener('keydown',this.keyboardEvent.bind(this))
+    }
+    keyboardEvent(e:KeyboardEvent){
+        this.direction = e.key
+    }
+
 }
+export default GameControl
