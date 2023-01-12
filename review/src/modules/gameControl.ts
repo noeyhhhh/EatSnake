71dc46a95b725990d2690f30d03340684e60b0e9
noeyhhh <lunihaoNB@outlook.com>
2023-01-12 17:59:48 +0800

    review

--- review/src/modules/gameControl.ts
+++ review/src/modules/gameControl.ts
@@ -7,6 +7,7 @@ class GameControl{
     scorepanel:scorePanel
     snake:Snake
     direction:string = ''
+    isAlive = true
     constructor() {
         this.food = new Food()
         this.scorepanel = new scorePanel()
@@ -16,10 +17,48 @@ class GameControl{
     }
     initGame(){
         document.addEventListener('keydown',this.keyboardEvent.bind(this))
+        this.run()
     }
     keyboardEvent(e:KeyboardEvent){
         this.direction = e.key
     }
-
+    run(){
+        let X = this.snake.X
+        let Y = this.snake.Y
+        switch(this.direction){
+            case "ArrowRight":
+                X += 10
+                console.log('1')
+                break
+            case "ArrowLeft":
+                X -= 10
+                console.log('1')
+                break
+            case "ArrowUp":
+                Y -= 10
+                console.log('1')
+                break
+            case "ArrowDown":
+                Y += 10
+                console.log('1')
+                break
+        }
+        try{
+            this.snake.X = X
+            this.snake.Y =Y
+        } catch (e:any){
+            alert(e.message)
+            this.isAlive = false
+        }
+        this.checkEatFood()
+        this.isAlive && setTimeout(this.run.bind(this),300-(this.scorepanel.level)*10)
+    }
+    checkEatFood(){
+        if(this.snake.X === this.food.foodX && this.snake.Y === this.food.foodY){
+            this.scorepanel.addScore()
+            this.food.foodChange()
+            this.snake.addBody()
+        }
+    }
 }
 export default GameControl
--- review/src/modules/snake.ts
+++ review/src/modules/snake.ts
@@ -1,9 +1,11 @@
 class Snake{
+    snakeEle:HTMLElement
     snakeHead:HTMLElement
     snakeBodies:HTMLCollection
     constructor() {
+        this.snakeEle = document.getElementById('snake')!
         this.snakeHead = document.querySelector('#snake>div') as HTMLElement
-        this.snakeBodies = document.getElementById('snake')!.getElementsByTagName('div')
+        this.snakeBodies = this.snakeEle.getElementsByTagName('div')
     }
     get X(){
         return this.snakeHead.offsetLeft
@@ -12,10 +14,30 @@ class Snake{
         return this.snakeHead.offsetTop
     }
     set X(val){
+        if(val<0 || val>290){
+            throw new Error('失败!撞墙了')
+        }
+        this.moveBody()
         this.snakeHead.style.left = val + 'px'
     }
     set Y(val){
+        if(val<0 || val>290){
+            throw new Error('失败!撞墙了')
+        }
+        this.moveBody()
         this.snakeHead.style.top = val + 'px'
+    }
+    addBody(){
+        let newDiv:HTMLElement = document.createElement('div')
+        this.snakeEle.insertAdjacentElement('beforeend',newDiv)
+    }
+    moveBody(){
+        for(let i = this.snakeBodies.length -1 ;i>0; i--){
+            let X = (this.snakeBodies[i-1] as HTMLElement).offsetLeft;
+            let Y = (this.snakeBodies[i-1] as HTMLElement).offsetTop;
+            (this.snakeBodies[i] as HTMLElement).style.left = X + 'px';
+            (this.snakeBodies[i] as HTMLElement).style.top = Y + 'px';
+        }
     }
 }
 export default Snake
